df6c3c304044108edd85a2335cc6c2eeb0d8ac4d
xtrphy <perepeckinvlad@gmail.com>
2025-09-05 18:49:46 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+    return (
+        <div className=''>
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div className='counter'>
+            <div className='btns'>
+                <button className='btn'>+</button>
+                <button className='btn'>-</button>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
